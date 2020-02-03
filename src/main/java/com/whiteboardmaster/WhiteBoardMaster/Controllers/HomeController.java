22d8e3235b8cfca4a918db96eb675a3630cefc2e
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-03 14:24:18 -0800

    update homecontroller

--- src/main/java/com/whiteboardmaster/WhiteBoardMaster/Controllers/HomeController.java
+++ src/main/java/com/whiteboardmaster/WhiteBoardMaster/Controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.whiteboardmaster.WhiteBoardMaster.Controllers;
 
+import com.whiteboardmaster.WhiteBoardMaster.Models.ApplicationUser;
 import com.whiteboardmaster.WhiteBoardMaster.Models.ApplicationUserRepository;
 import com.whiteboardmaster.WhiteBoardMaster.Models.DiagramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -18,13 +20,14 @@ public class HomeController {
     DiagramRepository diagramRepository;
 
     @GetMapping("/")
-    public String displayIndex(Principal p, Model m) {
+    public String getHome(Principal p, Model m){
 
-//        if (p != null) {
+        if(p == null){
+            m.addAttribute("username", "New User");
+        } else {
+            m.addAttribute("username", p.getName());
+        }
 
-            // add user and list to model
-            m.addAttribute("user", "new user");
-//        }
         return "home";
     }
 }
